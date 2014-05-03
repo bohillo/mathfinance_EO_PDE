5e95ef79b3887c701a9b38982fcdf9aea3ab229d
Piotr Bochnia <pb305049@students.mimuw.edu.pl>
2014-05-03 15:59:11 +0200

    Dodane greeks (poza theta) do DM_doacall

--- exotic_options.m
+++ exotic_options.m
@@ -52,8 +52,8 @@ function [result_DM_doamcall] = DM_doamcall(F_bid, F_ask, barrier, strike, monit
  %%%%%%%%%%%%%%%
  
  % monitoring dates adjustment
- 
- Mt_adj = round(100000 * tau / gcd(round(100000 * monitoring_t)(2), round(100000 * monitoring_t)(3)));
+ MF_precision = 100000;
+ Mt_adj = round(MF_precision * tau / gcd(round(MF_precision * monitoring_t)(2), round(MF_precision * monitoring_t)(3)));
  Mt = ceil(Mt / Mt_adj) * Mt_adj;
  dt = tau / Mt;
  monitoring_k = round(monitoring_t / dt) + 1;
@@ -69,7 +69,7 @@ function [result_DM_doamcall] = DM_doamcall(F_bid, F_ask, barrier, strike, monit
  xmax = xmax_factor * F * DF_d / DF_f ;
  x = linspace(xmin, xmax, Mx + 1);
  t = linspace(0, tau, Mt + 1);
- 
+ dx = (xmax - xmin)/Mx;
  sigma = vol(t, x, strike, tau);
  term_cond = max(x - strike, 0);
  left_cond = zeros(Mt + 1, 1);
@@ -80,15 +80,58 @@ function [result_DM_doamcall] = DM_doamcall(F_bid, F_ask, barrier, strike, monit
  
  
  V = solve_BS_PDE (r0, r1, sigma, term_cond, left_cond, right_cond, left_bound, right_bound, xmin, xmax, tau, Mx, Mt);
- result_DM_doamcall = zeros(1,7);
- result_DM_doamcall(1) = interp1(x, V, F * DF_d / DF_f);
-
  
- endfunction
+ result_DM_doamcall = zeros(1,7);
+ 
+ 
+ %% price
+ result_DM_doamcall(1) = interp1(x, V, F * DF_d / DF_f);
+ 
+ %% For Greeks
+
+ %% spot delta
+ result_DM_doamcall(2) = \ 
+ ( interp1(x, V, F * DF_d / DF_f + dx) \ 
+  -interp1(x, V, F * DF_d / DF_f - dx) ) / (2 * dx);
+ 
+ %% forward delta
+  result_DM_doamcall(3) = \ 
+ ( interp1(x, V, (F + dx) * DF_d / DF_f) \ 
+  -interp1(x, V, (F - dx) * DF_d / DF_f) ) / (2 * dx);
+ 
+ %% spot gamma
+  result_DM_doamcall(4) = \ 
+ ( interp1(x, V, F * DF_d / DF_f + dx) \ 
+  -2 * interp1(x, V, F * DF_d / DF_f) \
+  +interp1(x, V, F * DF_d / DF_f - dx)) / dx^2;
+ 
+ %% forward gamma
+ result_DM_doamcall(5) = \ 
+ ( interp1(x, V, (F + dx) * DF_d / DF_f) \ 
+  -2 * interp1(x, V, F * DF_d / DF_f) \
+  +interp1(x, V, (F - dx) * DF_d / DF_f) ) / dx^2;
+ 
+ %% theta
+ issue_date2 = day_shift(issue_date, FC_DOM, 1);
+ % liczyć to tak, czy zwracać z solve_BS_PDE dla dwóch chwil
+ result_DM_doamcall(7) = 9999;
+
+ %% vega
+ dsigma = 0.001;
+ V1 = solve_BS_PDE (r0, r1, sigma + dsigma, term_cond, left_cond, right_cond, left_bound, right_bound, xmin, xmax, tau, Mx, Mt);
+ V2 = solve_BS_PDE (r0, r1, sigma - dsigma, term_cond, left_cond, right_cond, left_bound, right_bound, xmin, xmax, tau, Mx, Mt);
+ 
+ result_DM_doamcall(7) = \ 
+ ( interp1(x, V1, F * DF_d / DF_f) \ 
+  -interp1(x, V2, F * DF_d / DF_f) ) / (2 * dsigma);
+ 
+
+endfunction
 
 
 function V = solve_BS_PDE (r0, r1, sigma, term_cond, left_cond, \
-			   right_cond, left_bound, right_bound, xmin, xmax, T, Mx, Mt)
+			   right_cond, left_bound, right_bound, xmin, xmax, T, \
+			   Mx, Mt)
 	 	 	 
 	 V = term_cond';
 	 x = linspace(xmin, xmax, Mx + 1)';
